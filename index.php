afecfa1aaa7925262ef67cbf6c86a082b061e9ee
Rafa <rafapessoa623@gmail.com>
2025-11-03 21:15:43 -0300

    apenas a base

--- index.php
+++ index.php
@@ -3,7 +3,16 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>
+
+    <p>  cardapio online  </p>
+
+
+    <h1>entrar</h1><button>entrar</button>
+    <br>
+    <h1>entrar como adm</h1><button>entrar como adm</button>
+
+    </title>
 </head>
 <body>
     
