9c3bef7542e445c83d267bd1384d78a9f5078b90
Rafa <rafapessoa623@gmail.com>
2025-11-06 13:37:46 -0300

    base de menu (sem funcionar ainda)

--- index.php
+++ index.php
@@ -1,20 +1,23 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
+
+    <link rel="stylesheet" href="estilo.css"></link>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title> cardapio </title>
 
-    <p>  cardapio online  </p>
-
-
-    <h1>entrar</h1><button>entrar</button>
-    <br>
-    <h1>entrar como adm</h1><button>entrar como adm</button>
-
-    
 </head>
 <body>
     
+<p>  cardapio online  </p>
+
+
+    <h1>Cliente?</h1>
+    <button>entrar como cliente</button>
+
+    <h1>administrador da pagina?</h1>
+    <button>entrar como adm</button>
+
 </body>
 </html>
